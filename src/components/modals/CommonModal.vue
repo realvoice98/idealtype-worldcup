8af4bd7f463ee29620031cf29ab8c3b8ba96b99a
eugene_cha <detergentyoon@gmail.com>
2024-10-14 00:41:32 +0900

    공통 모달 헤더 및 버튼 추가 생성 로직 추가

--- src/components/modals/CommonModal.vue
+++ src/components/modals/CommonModal.vue
@@ -1,11 +1,15 @@
 <template>
-  <button @click="openModal">OPEN</button>
-  <div class="modal-wrap" v-show="isModalVisible" @click="openModal">
+  <button @click="openModal">{{ openBtnText }}</button>
+  <div class="modal-wrap" v-show="isModalVisible" @click="openModal"> <!-- dim 클릭 시에도 닫기 -->
     <div class="modal-container" @click.stop> <!-- 이벤트 버블링 방지 -->
-<!--      <div class="modal-header">-->
+      <div class="modal-header" v-if="title"> <!-- 제목 생략 가능 -->
+        <h2>{{ title }}</h2>
+      </div>
       <div class="btn-modal">
-        <button @click="openModal">닫기</button>
-        <button @click="openModal">확인</button>
+        <p>{{ content }}</p>
+        <span v-for="(button, index) in buttons" :key="index">
+          <button @click="button.callback">{{ button.text }}</button>
+        </span>
       </div>
     </div>
   </div>
@@ -14,6 +18,21 @@
 <script>
 export default {
   name: "CommonModal",
+  props: {
+    openBtnText: {
+      type: String,
+    },
+    title: {
+      type: String,
+    },
+    content: {
+      type: String,
+    },
+    buttons: {
+      type: Array,
+      default: () => [],
+    }
+  },
   data() {
     return {
       isModalVisible: false,
