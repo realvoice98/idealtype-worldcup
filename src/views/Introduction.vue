2dca25f4771545dd1b8c26a496ddda1b471f3761
KSH-99 <kk00011157@gmail.com>
2024-10-17 13:57:24 +0900

    TypeIt 추가, 소개 페이지 타이틀 추가(미완)

--- src/views/Introduction.vue
+++ src/views/Introduction.vue
@@ -1,13 +1,33 @@
 <template>
+  <div class="intro-container">
+    <div class="intro-title">
+      <h1 id="title"></h1>
+    </div>
+    <div class="intro-content">
 
+    </div>
+  </div>
 </template>
 
 <script>
+import TypeIt from "typeit";
+
 export default {
-  name: "Introduction"
-}
+  name: "Introduction",
+  mounted() {
+    new TypeIt("#title", {
+      strings: "이상형 월드컵",
+      speed: 50,
+      waitUntilVisible: true,
+      loop: true
+    }).pause(2000).delete(7).type("Ideal Type World Cup").go();
+  },
+};
 </script>
 
 <style scoped>
-
+/*
+fucking css
+존나 어렵네
+*/
 </style>
