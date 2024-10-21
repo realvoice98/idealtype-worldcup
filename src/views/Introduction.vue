97d246f745901141714ab973f670a66cf7eb03bd
KSH-99 <kk00011157@gmail.com>
2024-10-21 16:40:48 +0900

    소개 페이지 임시텍스트 변경

--- src/views/Introduction.vue
+++ src/views/Introduction.vue
@@ -5,103 +5,110 @@
     </div>
     <div class="intro-content">
       <section class="text-content" data-scroll>
-        <h1 id="subtitle">손톱만큼</h1>
-        <p>누군가 내게</p>
-        <p>“신두형을 얼마나 사랑했나요” 하고 묻는다면,</p>
-        <p>나는 외면하며 “손톱만큼이요” 라고 할 것이다</p>
-        <p>하지만 돌아서서는,</p>
-        <p>잘라내도 잘라내도 평생 자라나고야 마는</p>
-        <p>내 손톱을 보고 마음이 저려 펑펑 울지도 모른다</p>
+        <h1 id="subtitle">Lorem ipsum</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        <p>Nisl tincidunt eget nullam non.</p>
+        <p>Quis hendrerit dolor magna eget est lorem ipsum dolor sit.</p>
+        <p>Volutpat odio facilisis mauris sit amet massa.</p>
+        <p>Commodo odio aenean sed adipiscing diam donec adipiscing tristique.</p>
+        <p>Mi eget mauris pharetra et.</p>
+        <p>Non tellus orci ac auctor augue.</p>
+        <p>Elit at imperdiet dui accumsan sit.</p>
+        <p>Ornare arcu dui vivamus arcu felis.</p>
+        <p>Egestas integer eget aliquet nibh praesent.</p>
+        <p>In hac habitasse platea dictumst quisque sagittis purus.</p>
+        <p>Pulvinar elementum integer enim neque volutpat ac.</p>
+        <p>Senectus et netus et malesuada.</p>
+        <p>Nunc pulvinar sapien et ligula ullamcorper malesuada proin.</p>
+        <p>Neque convallis a cras semper auctor.</p>
+        <p>Libero id faucibus nisl tincidunt eget.</p>
+        <p>Leo a diam sollicitudin tempor id.</p>
+        <p>A lacus vestibulum sed arcu non odio euismod lacinia.</p>
+        <p>In tellus integer feugiat scelerisque.</p>
+        <p>Feugiat in fermentum posuere urna nec tincidunt praesent.</p>
+        <p>Porttitor rhoncus dolor purus non enim praesent elementum facilisis.</p>
+        <p>Nisi scelerisque eu ultrices vitae auctor eu augue ut lectus.</p>
+        <p>Ipsum faucibus vitae aliquet nec ullamcorper sit amet risus.</p>
+        <p>Et malesuada fames ac turpis egestas sed.</p>
+        <p>Sit amet nisl suscipit adipiscing bibendum est ultricies.</p>
+        <p>Arcu ac tortor dignissim convallis aenean et tortor at.</p>
+        <p>Pretium viverra suspendisse potenti nullam ac tortor vitae purus.</p>
+        <p>Eros donec ac odio tempor orci dapibus ultrices.</p>
+        <p>Elementum nibh tellus molestie nunc.</p>
+        <p>Et magnis dis parturient montes nascetur.</p>
+        <p>Est placerat in egestas erat imperdiet.</p>
+        <p>Consequat interdum varius sit amet mattis vulputate enim.</p>
+        <p>Sit amet nulla facilisi morbi tempus.</p>
+        <p>Nulla facilisi cras fermentum odio eu.</p>
+        <p>Etiam erat velit scelerisque in dictum non consectetur a erat.</p>
+        <p>Enim nulla aliquet porttitor lacus luctus accumsan tortor posuere.</p>
+        <p>Ut sem nulla pharetra diam.</p>
+        <p>Fames ac turpis egestas maecenas.</p>
+        <p>Bibendum neque egestas congue quisque egestas diam.</p>
+        <p>Laoreet id donec ultrices tincidunt arcu non sodales neque.</p>
+        <p>Eget felis eget nunc lobortis mattis aliquam faucibus purus.</p>
+        <p>Faucibus interdum posuere lorem ipsum dolor sit.</p>
+        <p>Et netus et malesuada fames ac.</p>
+        <p>Erat pellentesque adipiscing commodo elit at imperdiet dui accumsan.</p>
+        <p>Sodales neque sodales ut etiam sit amet nisl purus in.</p>
+        <p>Maecenas volutpat blandit aliquam etiam.</p>
+        <p>Sit amet luctus venenatis lectus magna fringilla urna porttitor rhoncus.</p>
+        <p>Egestas purus viverra accumsan in nisl.</p>
+        <p>Semper feugiat nibh sed pulvinar proin.</p>
+        <p>Duis convallis convallis tellus id interdum velit laoreet.</p>
       </section>
       <section class="text-content" data-scroll>
-        <h1 id="subtitle">친절한 이웃</h1>
-        <p>버스를 탔을 때,</p>
-        <p>기사님이 의아한 표정으로 물었다.</p>
-        <p>“학생, 1명인데 왜 2명 찍어?”</p>
-        <p>“제 마음속에는 언제나 크리스티아노가 살고있기 때문이죠.”</p>
-        <p>기사님이 웃으며 말했다.</p>
-        <p>“학생, 우리들의 친절한 이웃 크리스티아노는 요금을 안 받는단다.”</p>
-      </section>
-      <section class="text-content" data-scroll>
-        <h1 id="subtitle">하트 접기</h1>
-        <p>진짜 안되겠어요. 저 오늘부로 신두형에 대한 마음 접습니다.</p>
-        <p>그 다음에 끝선에 맞추어 다시 반 접습니다.</p>
-        <p>뒤로 돌려 양쪽 모두 펼칩니다.</p>
-        <p>끝 부분을 살짝 접고선 중심 선에 맞춰 위로 올려 접은 뒤 뒤집으면 예쁜 우리 신두형 하트 접기 완성!!❤️❤️</p>
-      </section>
-      <section class="text-content" data-scroll>
-        <h1 id="subtitle">내 심장에</h1>
-        <p>“미국은 어디있지?”</p>
-        <p>“북위 24-48, 경도 67-125도,</p>
-        <p>북아메리카에.”</p>
-        <p>“대한민국은?”</p>
-        <p>“동경 127도, 북위 37도,</p>
-        <p>동북아시아에.”</p>
-        <p>“호날두는-”</p>
-        <p>“여기, 내 심장에.”</p>
-      </section>
-      <section class="text-content" data-scroll>
-        <h1 id="subtitle">팬</h1>
-        <p>세상에 70억명의 호날두 팬이 있다면, 나는 그들 중 한 명일 것이다.</p>
-        <p>세상에 1억명의 호날두 팬이 있다면, 나 또한 그들 중 한 명일 것이다.</p>
-        <p>세상에 천만 명의 호날두 팬이 있다면, 나는 여전히 그들 중 한 명일 것이다.</p>
-        <p>세상에 백 명의 호날두 팬이 있다면, 나는 아직도 그들 중 한 명일 것이다.</p>
-        <p>세상에 한 명의 호날두 팬이 있다면, 그 사람은 아마도 나일 것이다.</p>
-        <p>세상에 단 한 명의 호날두 팬도 없다면, 나는 그제서야 이 세상에 없는 것이다.</p>
-        <p>호날두, 나의 사랑.</p>
-        <p>호날두, 나의 빛.</p>
-        <p>호날두, 나의 어둠.</p>
-        <p>호날두, 나의 삶.</p>
-        <p>호날두, 나의 기쁨.</p>
-        <p>호날두, 나의 슬픔.</p>
-        <p>호날두, 나의 고통.</p>
-        <p>호날두, 나의 안식.</p>
-        <p>호날두, 나.</p>
-      </section>
-      <section class="text-content" data-scroll>
-        <h1 id="subtitle">SIU</h1>
-        <p>호날두 좋으면 만세 이딴말 왜 하는거임?</p>
-        <p>나 그럼 어떡하라고 계속 손들고 있으라고?</p>
-        <p>나 그럼 아무것도 못해</p>
-        <p>나 그럼 다 허락 맡아야 돼?</p>
-        <p>나 그럼 계속 손들고 있어야 되냐고</p>
-        <p>나 그럼 밥도 못먹고 화장실도 못가고</p>
-        <p>젠장, 또 신두형이야. 이 게시글만 보고 자려고 했는데, 신두형을 보고 말았어. 이제 나는 외쳐야만 해...</p>
-        <p>외치면 잠이 확 깨 버릴 걸 알면서도, 나는 외쳐. 그것이 신두형을 목도한 자의 사명이다. 자, 외치겠어.</p>
-        <p>SIUUUUUUUUUUUUU!!!!!!!!</p>
-      </section>
-      <section class="text-content" data-scroll>
-        <h1 id="subtitle">카페</h1>
-        <p>어제 호날두 카페 다녀왔습니다</p>
-        <p>호날두 카페가 열린 건 아니고요</p>
-        <p>그냥 카페에서 호날두 생각했습니다</p>
-        <p>카페에 간 건 아니고요</p>
-        <p>그냥 집에서 커피를 마셨습니다</p>
-        <p>사실 커피도 안 마셨습니다</p>
-        <p>그냥 호날두 상태입니다</p>
-      </section>
-      <section class="text-content" data-scroll>
-        <h1 id="subtitle">이름</h1>
-        <p>호날두라는 이름은</p>
-        <p>누가 지어냈는지 모르겠어요.</p>
-        <p>“호…날…두…” 하고 불러 보면</p>
-        <p>금시로 따스해 오는 내 마음.</p>
-        <p>“호…날…두” 하고 불러 보면</p>
-        <p>“오오-” 하고 들려 오는 듯한 목소리.</p>
-        <p>참말 이 세상에서</p>
-        <p>하나밖에 없는 이름.</p>
-        <p>바위도 오래 되면 깎여지는데</p>
-        <p>해같이 달같이 오랠</p>
-        <p>호날두의 이름.</p>
-      </section>
-      <section class="text-content" data-scroll>
-        <h1 id="subtitle">벗</h1>
-        <p>호날두 뒤에서 걷지 마라.</p>
-        <p>호날두는 그대를 이끌지 않을 수도 있다.</p>
-        <p>호날두 앞에서 걷지 마라.</p>
-        <p>호날두는 그대를 따르지 않을 수도 있다.</p>
-        <p>다만 호날두 옆에서 걸으라.</p>
-        <p>호날두의 벗이 될 수 있도록.</p>
+        <h1 id="subtitle">Lorem ipsum</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        <p>Nisl tincidunt eget nullam non.</p>
+        <p>Quis hendrerit dolor magna eget est lorem ipsum dolor sit.</p>
+        <p>Volutpat odio facilisis mauris sit amet massa.</p>
+        <p>Commodo odio aenean sed adipiscing diam donec adipiscing tristique.</p>
+        <p>Mi eget mauris pharetra et.</p>
+        <p>Non tellus orci ac auctor augue.</p>
+        <p>Elit at imperdiet dui accumsan sit.</p>
+        <p>Ornare arcu dui vivamus arcu felis.</p>
+        <p>Egestas integer eget aliquet nibh praesent.</p>
+        <p>In hac habitasse platea dictumst quisque sagittis purus.</p>
+        <p>Pulvinar elementum integer enim neque volutpat ac.</p>
+        <p>Senectus et netus et malesuada.</p>
+        <p>Nunc pulvinar sapien et ligula ullamcorper malesuada proin.</p>
+        <p>Neque convallis a cras semper auctor.</p>
+        <p>Libero id faucibus nisl tincidunt eget.</p>
+        <p>Leo a diam sollicitudin tempor id.</p>
+        <p>A lacus vestibulum sed arcu non odio euismod lacinia.</p>
+        <p>In tellus integer feugiat scelerisque.</p>
+        <p>Feugiat in fermentum posuere urna nec tincidunt praesent.</p>
+        <p>Porttitor rhoncus dolor purus non enim praesent elementum facilisis.</p>
+        <p>Nisi scelerisque eu ultrices vitae auctor eu augue ut lectus.</p>
+        <p>Ipsum faucibus vitae aliquet nec ullamcorper sit amet risus.</p>
+        <p>Et malesuada fames ac turpis egestas sed.</p>
+        <p>Sit amet nisl suscipit adipiscing bibendum est ultricies.</p>
+        <p>Arcu ac tortor dignissim convallis aenean et tortor at.</p>
+        <p>Pretium viverra suspendisse potenti nullam ac tortor vitae purus.</p>
+        <p>Eros donec ac odio tempor orci dapibus ultrices.</p>
+        <p>Elementum nibh tellus molestie nunc.</p>
+        <p>Et magnis dis parturient montes nascetur.</p>
+        <p>Est placerat in egestas erat imperdiet.</p>
+        <p>Consequat interdum varius sit amet mattis vulputate enim.</p>
+        <p>Sit amet nulla facilisi morbi tempus.</p>
+        <p>Nulla facilisi cras fermentum odio eu.</p>
+        <p>Etiam erat velit scelerisque in dictum non consectetur a erat.</p>
+        <p>Enim nulla aliquet porttitor lacus luctus accumsan tortor posuere.</p>
+        <p>Ut sem nulla pharetra diam.</p>
+        <p>Fames ac turpis egestas maecenas.</p>
+        <p>Bibendum neque egestas congue quisque egestas diam.</p>
+        <p>Laoreet id donec ultrices tincidunt arcu non sodales neque.</p>
+        <p>Eget felis eget nunc lobortis mattis aliquam faucibus purus.</p>
+        <p>Faucibus interdum posuere lorem ipsum dolor sit.</p>
+        <p>Et netus et malesuada fames ac.</p>
+        <p>Erat pellentesque adipiscing commodo elit at imperdiet dui accumsan.</p>
+        <p>Sodales neque sodales ut etiam sit amet nisl purus in.</p>
+        <p>Maecenas volutpat blandit aliquam etiam.</p>
+        <p>Sit amet luctus venenatis lectus magna fringilla urna porttitor rhoncus.</p>
+        <p>Egestas purus viverra accumsan in nisl.</p>
+        <p>Semper feugiat nibh sed pulvinar proin.</p>
+        <p>Duis convallis convallis tellus id interdum velit laoreet.</p>
       </section>
     </div>
   </div>
@@ -110,6 +117,8 @@
 <script>
 import TypeIt from "typeit";
 import ScrollOut from "scroll-out";
+
+//TODO: 소개 페이지 텍스트를 하드코딩할지 의논 필요. 주기적으로 바꾸지 않으면 그냥 하드코딩이 나을 듯 싶음.
 
 export default {
   name: "Introduction",
@@ -143,6 +152,8 @@ export default {
 </script>
 
 <style scoped>
+
+
  .intro-container{
    padding: 20px;
  }
@@ -151,7 +162,8 @@ export default {
    padding: 10px;
  }
 
- .text-content {
+ .text-content {/*FIXME: 너비 값 수정 필요*/
+   font-size: large;
    margin-bottom: 300px;
    border-radius: 8px;
    box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
