6af88e8a8b88cca88ab46f2596a6734d1198725a
KSH-99 <kk00011157@gmail.com>
2024-10-29 02:45:18 +0900

    introduction 더미 이미지 추가

--- src/views/Introduction.vue
+++ src/views/Introduction.vue
@@ -4,111 +4,89 @@
       <h1 id="title"></h1>
     </div>
     <div class="intro-content">
-      <section class="text-content" data-scroll>
-        <h1 id="subtitle">Lorem ipsum</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-        <p>Nisl tincidunt eget nullam non.</p>
-        <p>Quis hendrerit dolor magna eget est lorem ipsum dolor sit.</p>
-        <p>Volutpat odio facilisis mauris sit amet massa.</p>
-        <p>Commodo odio aenean sed adipiscing diam donec adipiscing tristique.</p>
-        <p>Mi eget mauris pharetra et.</p>
-        <p>Non tellus orci ac auctor augue.</p>
-        <p>Elit at imperdiet dui accumsan sit.</p>
-        <p>Ornare arcu dui vivamus arcu felis.</p>
-        <p>Egestas integer eget aliquet nibh praesent.</p>
-        <p>In hac habitasse platea dictumst quisque sagittis purus.</p>
-        <p>Pulvinar elementum integer enim neque volutpat ac.</p>
-        <p>Senectus et netus et malesuada.</p>
-        <p>Nunc pulvinar sapien et ligula ullamcorper malesuada proin.</p>
-        <p>Neque convallis a cras semper auctor.</p>
-        <p>Libero id faucibus nisl tincidunt eget.</p>
-        <p>Leo a diam sollicitudin tempor id.</p>
-        <p>A lacus vestibulum sed arcu non odio euismod lacinia.</p>
-        <p>In tellus integer feugiat scelerisque.</p>
-        <p>Feugiat in fermentum posuere urna nec tincidunt praesent.</p>
-        <p>Porttitor rhoncus dolor purus non enim praesent elementum facilisis.</p>
-        <p>Nisi scelerisque eu ultrices vitae auctor eu augue ut lectus.</p>
-        <p>Ipsum faucibus vitae aliquet nec ullamcorper sit amet risus.</p>
-        <p>Et malesuada fames ac turpis egestas sed.</p>
-        <p>Sit amet nisl suscipit adipiscing bibendum est ultricies.</p>
-        <p>Arcu ac tortor dignissim convallis aenean et tortor at.</p>
-        <p>Pretium viverra suspendisse potenti nullam ac tortor vitae purus.</p>
-        <p>Eros donec ac odio tempor orci dapibus ultrices.</p>
-        <p>Elementum nibh tellus molestie nunc.</p>
-        <p>Et magnis dis parturient montes nascetur.</p>
-        <p>Est placerat in egestas erat imperdiet.</p>
-        <p>Consequat interdum varius sit amet mattis vulputate enim.</p>
-        <p>Sit amet nulla facilisi morbi tempus.</p>
-        <p>Nulla facilisi cras fermentum odio eu.</p>
-        <p>Etiam erat velit scelerisque in dictum non consectetur a erat.</p>
-        <p>Enim nulla aliquet porttitor lacus luctus accumsan tortor posuere.</p>
-        <p>Ut sem nulla pharetra diam.</p>
-        <p>Fames ac turpis egestas maecenas.</p>
-        <p>Bibendum neque egestas congue quisque egestas diam.</p>
-        <p>Laoreet id donec ultrices tincidunt arcu non sodales neque.</p>
-        <p>Eget felis eget nunc lobortis mattis aliquam faucibus purus.</p>
-        <p>Faucibus interdum posuere lorem ipsum dolor sit.</p>
-        <p>Et netus et malesuada fames ac.</p>
-        <p>Erat pellentesque adipiscing commodo elit at imperdiet dui accumsan.</p>
-        <p>Sodales neque sodales ut etiam sit amet nisl purus in.</p>
-        <p>Maecenas volutpat blandit aliquam etiam.</p>
-        <p>Sit amet luctus venenatis lectus magna fringilla urna porttitor rhoncus.</p>
-        <p>Egestas purus viverra accumsan in nisl.</p>
-        <p>Semper feugiat nibh sed pulvinar proin.</p>
-        <p>Duis convallis convallis tellus id interdum velit laoreet.</p>
+      <section class="text-content left" data-scroll>
+        <div class="image-container left" data-scroll>
+          <img src="@/assets/logo.png" alt="Left Image" />
+        </div>
+        <div class="text-box right">
+          <h1 id="subtitle">Lorem ipsum</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.</p>
+          <p>Nisl tincidunt eget nullam non.</p>
+          <p>Quis hendrerit dolor magna eget est lorem ipsum dolor sit.</p>
+          <p>Volutpat odio facilisis mauris sit amet massa.</p>
+          <p>Commodo odio aenean sed adipiscing diam donec adipiscing tristique.</p>
+          <p>Mi eget mauris pharetra et.</p>
+          <p>Non tellus orci ac auctor augue.</p>
+          <p>Elit at imperdiet dui accumsan sit.</p>
+          <p>Ornare arcu dui vivamus arcu felis.</p>
+          <p>Egestas integer eget aliquet nibh praesent.</p>
+          <p>In hac habitasse platea dictumst quisque sagittis purus.</p>
+          <p>Pulvinar elementum integer enim neque volutpat ac.</p>
+          <p>Senectus et netus et malesuada.</p>
+          <p>Nunc pulvinar sapien et ligula ullamcorper malesuada proin.</p>
+          <p>Neque convallis a cras semper auctor.</p>
+          <p>Libero id faucibus nisl tincidunt eget.</p>
+          <p>Leo a diam sollicitudin tempor id.</p>
+          <p>A lacus vestibulum sed arcu non odio euismod lacinia.</p>
+          <p>In tellus integer feugiat scelerisque.</p>
+          <p>Feugiat in fermentum posuere urna nec tincidunt praesent.</p>
+        </div>
       </section>
-      <section class="text-content" data-scroll>
-        <h1 id="subtitle">Lorem ipsum</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-        <p>Nisl tincidunt eget nullam non.</p>
-        <p>Quis hendrerit dolor magna eget est lorem ipsum dolor sit.</p>
-        <p>Volutpat odio facilisis mauris sit amet massa.</p>
-        <p>Commodo odio aenean sed adipiscing diam donec adipiscing tristique.</p>
-        <p>Mi eget mauris pharetra et.</p>
-        <p>Non tellus orci ac auctor augue.</p>
-        <p>Elit at imperdiet dui accumsan sit.</p>
-        <p>Ornare arcu dui vivamus arcu felis.</p>
-        <p>Egestas integer eget aliquet nibh praesent.</p>
-        <p>In hac habitasse platea dictumst quisque sagittis purus.</p>
-        <p>Pulvinar elementum integer enim neque volutpat ac.</p>
-        <p>Senectus et netus et malesuada.</p>
-        <p>Nunc pulvinar sapien et ligula ullamcorper malesuada proin.</p>
-        <p>Neque convallis a cras semper auctor.</p>
-        <p>Libero id faucibus nisl tincidunt eget.</p>
-        <p>Leo a diam sollicitudin tempor id.</p>
-        <p>A lacus vestibulum sed arcu non odio euismod lacinia.</p>
-        <p>In tellus integer feugiat scelerisque.</p>
-        <p>Feugiat in fermentum posuere urna nec tincidunt praesent.</p>
-        <p>Porttitor rhoncus dolor purus non enim praesent elementum facilisis.</p>
-        <p>Nisi scelerisque eu ultrices vitae auctor eu augue ut lectus.</p>
-        <p>Ipsum faucibus vitae aliquet nec ullamcorper sit amet risus.</p>
-        <p>Et malesuada fames ac turpis egestas sed.</p>
-        <p>Sit amet nisl suscipit adipiscing bibendum est ultricies.</p>
-        <p>Arcu ac tortor dignissim convallis aenean et tortor at.</p>
-        <p>Pretium viverra suspendisse potenti nullam ac tortor vitae purus.</p>
-        <p>Eros donec ac odio tempor orci dapibus ultrices.</p>
-        <p>Elementum nibh tellus molestie nunc.</p>
-        <p>Et magnis dis parturient montes nascetur.</p>
-        <p>Est placerat in egestas erat imperdiet.</p>
-        <p>Consequat interdum varius sit amet mattis vulputate enim.</p>
-        <p>Sit amet nulla facilisi morbi tempus.</p>
-        <p>Nulla facilisi cras fermentum odio eu.</p>
-        <p>Etiam erat velit scelerisque in dictum non consectetur a erat.</p>
-        <p>Enim nulla aliquet porttitor lacus luctus accumsan tortor posuere.</p>
-        <p>Ut sem nulla pharetra diam.</p>
-        <p>Fames ac turpis egestas maecenas.</p>
-        <p>Bibendum neque egestas congue quisque egestas diam.</p>
-        <p>Laoreet id donec ultrices tincidunt arcu non sodales neque.</p>
-        <p>Eget felis eget nunc lobortis mattis aliquam faucibus purus.</p>
-        <p>Faucibus interdum posuere lorem ipsum dolor sit.</p>
-        <p>Et netus et malesuada fames ac.</p>
-        <p>Erat pellentesque adipiscing commodo elit at imperdiet dui accumsan.</p>
-        <p>Sodales neque sodales ut etiam sit amet nisl purus in.</p>
-        <p>Maecenas volutpat blandit aliquam etiam.</p>
-        <p>Sit amet luctus venenatis lectus magna fringilla urna porttitor rhoncus.</p>
-        <p>Egestas purus viverra accumsan in nisl.</p>
-        <p>Semper feugiat nibh sed pulvinar proin.</p>
-        <p>Duis convallis convallis tellus id interdum velit laoreet.</p>
+      <section class="text-content right" data-scroll>
+        <div class="image-container right" data-scroll>
+          <img src="https://v-llage.s3.ap-northeast-2.amazonaws.com/1669707438698_%ED%81%B4%EB%A6%AC%EC%85%B0.png" alt="right Image" />
+        </div>
+        <div class="text-box left">
+          <h1 id="subtitle">Lorem ipsum</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.</p>
+          <p>Nisl tincidunt eget nullam non.</p>
+          <p>Quis hendrerit dolor magna eget est lorem ipsum dolor sit.</p>
+          <p>Volutpat odio facilisis mauris sit amet massa.</p>
+          <p>Commodo odio aenean sed adipiscing diam donec adipiscing tristique.</p>
+          <p>Mi eget mauris pharetra et.</p>
+          <p>Non tellus orci ac auctor augue.</p>
+          <p>Elit at imperdiet dui accumsan sit.</p>
+          <p>Ornare arcu dui vivamus arcu felis.</p>
+          <p>Egestas integer eget aliquet nibh praesent.</p>
+          <p>In hac habitasse platea dictumst quisque sagittis purus.</p>
+          <p>Pulvinar elementum integer enim neque volutpat ac.</p>
+          <p>Senectus et netus et malesuada.</p>
+          <p>Nunc pulvinar sapien et ligula ullamcorper malesuada proin.</p>
+          <p>Neque convallis a cras semper auctor.</p>
+          <p>Libero id faucibus nisl tincidunt eget.</p>
+          <p>Leo a diam sollicitudin tempor id.</p>
+          <p>A lacus vestibulum sed arcu non odio euismod lacinia.</p>
+          <p>In tellus integer feugiat scelerisque.</p>
+          <p>Feugiat in fermentum posuere urna nec tincidunt praesent.</p>
+        </div>
+      </section>
+      <section class="text-content left" data-scroll>
+        <div class="image-container left" data-scroll>
+          <img src="https://velog.velcdn.com/images%2Fhing%2Fpost%2Fd06f8ff0-f27b-497e-9d5f-06a6706e0d59%2Fimage.png" alt="Left Image" />
+        </div>
+        <div class="text-box right">
+          <h1 id="subtitle">Lorem ipsum</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.</p>
+          <p>Nisl tincidunt eget nullam non.</p>
+          <p>Quis hendrerit dolor magna eget est lorem ipsum dolor sit.</p>
+          <p>Volutpat odio facilisis mauris sit amet massa.</p>
+          <p>Commodo odio aenean sed adipiscing diam donec adipiscing tristique.</p>
+          <p>Mi eget mauris pharetra et.</p>
+          <p>Non tellus orci ac auctor augue.</p>
+          <p>Elit at imperdiet dui accumsan sit.</p>
+          <p>Ornare arcu dui vivamus arcu felis.</p>
+          <p>Egestas integer eget aliquet nibh praesent.</p>
+          <p>In hac habitasse platea dictumst quisque sagittis purus.</p>
+          <p>Pulvinar elementum integer enim neque volutpat ac.</p>
+          <p>Senectus et netus et malesuada.</p>
+          <p>Nunc pulvinar sapien et ligula ullamcorper malesuada proin.</p>
+          <p>Neque convallis a cras semper auctor.</p>
+          <p>Libero id faucibus nisl tincidunt eget.</p>
+          <p>Leo a diam sollicitudin tempor id.</p>
+          <p>A lacus vestibulum sed arcu non odio euismod lacinia.</p>
+          <p>In tellus integer feugiat scelerisque.</p>
+          <p>Feugiat in fermentum posuere urna nec tincidunt praesent.</p>
+        </div>
       </section>
     </div>
   </div>
@@ -132,27 +110,23 @@ export default {
 
     ScrollOut({
       onShown: function(el) {
-        new TypeIt(el.querySelector('#subtitle'),{
-          startDelay: 500,
-          cursor: false,
-        }).pause(2000).go();
+        if (el.querySelector('#subtitle')) {
+          new TypeIt(el.querySelector('#subtitle'), {
+            startDelay: 500,
+            cursor: false,
+          }).pause(2000).go();
+        }
 
-        // remove the class
-        el.classList.remove("animated");
-
-        // force reflow
-        void el.offsetWidth;
-
-        // re-add the animated cl
-        el.classList.add("animated");
+        if (el.querySelector(".scroll-image")) {
+          el.querySelector(".scroll-image").classList.add("animate-in");
+        }
       }
     });
-  },
+  }
 };
 </script>
 
 <style scoped>
-
 
  .intro-container{
    padding: 20px;
@@ -162,11 +136,10 @@ export default {
    padding: 10px;
  }
 
- .text-content {/*FIXME: 너비 값 수정 필요*/
-   font-size: large;
+ .text-content {
+   display: flex;
+   align-items: center;
    margin-bottom: 300px;
-   border-radius: 8px;
-   box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
  }
 
  [data-scroll] {
@@ -181,6 +154,57 @@ export default {
  }
  [data-scroll="out"] {
    opacity: 0;
+ }
+
+ .image-container {
+   flex-shrink: 0;
+   transition: opacity 1s ease, transform 1s ease;
+ }
+
+ .image-container.left {
+   margin-right: 20px;
+   transform: translateX(-20px);
+ }
+
+ .image-container.right {
+   margin-left: 20px;
+   transform: translateX(20px);
+ }
+
+ .image-container img {
+   width: 600px;
+   height: auto;
+   border-radius: 8px;
+   box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+ }
+
+ .image-container[data-scroll="in"] {
+   opacity: 1;
+   transform: translateX(0);
+ }
+
+ .image-container[data-scroll="out"] {
+   opacity: 0;
+ }
+
+ .text-box {
+   flex: 1;
+ }
+
+ .text-box.left {
+   text-align: left;
+ }
+
+ .text-box.right {
+   text-align: right;
+ }
+
+ .text-content.left {
+   flex-direction: row; /* 이미지를 왼쪽에, 텍스트를 오른쪽에 */
+ }
+
+ .text-content.right {
+   flex-direction: row-reverse; /* 이미지를 오른쪽에, 텍스트를 왼쪽에 */
  }
 
 </style>
