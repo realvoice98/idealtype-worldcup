0ff752d6b6f9ff54e98b57c85a9f6aca131adf1e
eugene-cha <detergentyoon@gmail.com>
2024-12-07 01:21:51 +0900

    로딩 스피너 추가

---
+++ src/components/LoadingSpinner.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="loading-overlay" v-if="visible">
+    <div class="loading-spinner"></div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'LoadingSpinner',
+    props: {
+      visible: {
+        type: Boolean,
+        default: true,
+      }
+    },
+  }
+</script>
+
+<style scoped>
+  .loading-overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: rgba(255, 255, 255, 0.8);
+    z-index: 9999;
+  }
+
+  .loading-spinner {
+    width: 50px;
+    height: 50px;
+    border: 5px solid rgba(0, 0, 0, 0.1);
+    border-top: 5px solid var(--theme);
+    border-radius: 50%;
+    animation: spin 1s linear infinite;
+  }
+
+  @keyframes spin {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+</style>
