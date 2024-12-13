01db37bb022aa91f0efe4e2f8a102e2b463c293f
eugene-cha <detergentyoon@gmail.com>
2024-12-13 20:45:33 +0900

    마이페이지 > 내 프로필 퍼블리싱 작업 완료

--- src/components/user/ProfileDetail.vue
+++ src/components/user/ProfileDetail.vue
@@ -5,21 +5,45 @@
     </div>
     <div class="profile-content">
       <div class="content-item">
+        <div class="label-wrap">
+          <span class="icon">abc</span>
+          <span class="label">닉네임</span>
+        </div>
         <p>{{ user.nickname }}</p>
       </div>
       <div class="content-item">
+        <div class="label-wrap">
+          <span class="icon">email</span>
+          <span class="label">이메일</span>
+        </div>
         <p>{{ user.email }}</p>
       </div>
       <div class="content-item">
-        <p>{{ user.gender }}</p> <!-- 성별은 포맷 -->
+        <div class="label-wrap">
+          <span class="icon">male</span>
+          <span class="label">성별</span>
+        </div>
+        <p>{{ formattedGender }}</p>
       </div>
       <div class="content-item">
+        <div class="label-wrap">
+          <span class="icon">cake</span>
+          <span class="label">생년월일</span>
+        </div>
         <p>{{ user.birthday }}</p>
       </div>
       <div class="content-item">
+        <div class="label-wrap">
+          <span class="icon">person_add</span>
+          <span class="label">가입일자</span>
+        </div>
         <p>{{ user.createdAt }}</p>
       </div>
       <div class="content-item">
+        <div class="label-wrap">
+          <span class="icon">update</span>
+          <span class="label">마지막 로그인 시간</span>
+        </div>
         <p>{{ user.lastLoginedAt }}</p>
       </div>
     </div>
@@ -54,6 +78,11 @@ export default {
       });
       // TODO END
     },
+    computed: {
+      formattedGender() {
+        return this.user.gender === 'M' ? '남자' : this.user.gender === 'F' ? '여자' : '알 수 없음';
+      },
+    },
     methods: {
     },
   };
@@ -61,26 +90,52 @@ export default {
 
 <style scoped>
   .profile-container {
-    background-color: red;
-
+    padding: 0;
+    width: 90%;
+    border: 1px solid var(--theme);
+    border-radius: 8px;
   }
 
   .profile-title {
+    padding: 10px 0;
     background-color: var(--theme);
+    border-radius: 8px 8px 0 0;
   }
 
-  .profile-content {
-    border: 2px solid lightgray;
-    padding: 0 2rem;
-  }
   .content-item {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    padding: 10px 1.5rem;
     border-bottom: 1px solid lightgray;
   }
   .content-item:last-child {
     border-bottom: none;
   }
-  p {
+  .content-item p {
+    text-align: left;
     padding: 0;
     margin: 0;
+  }
+
+  .label-wrap {
+    flex-direction: row;
+    display: flex;
+    margin-bottom: 0.5rem;
+  }
+  .icon {
+    font-size: 1.3rem;
+    color: dimgray;
+    margin-right: 0.5rem;
+  }
+  .label {
+    margin-right: 20px;
+    color: dimgray;
+  }
+
+  .until-bday {
+    color: var(--theme);
+    margin-left: 1.5rem;
+    font-size: 0.85rem;
   }
 </style>
