4b88e4b6fee10c2b10e24ade891da43b0a743ccb
eugene-cha <detergentyoon@gmail.com>
2024-12-15 18:33:58 +0900

    신고하기 버튼 - 이미지 > Material Icon 변경

--- src/components/buttons/ReportButton.vue
+++ src/components/buttons/ReportButton.vue
@@ -7,13 +7,15 @@
       :style="style"
       @click="report"
   >
-    <img
-        :src="require('@/assets/report-icon.png')"
-        alt="Report Icon"
-        @animationend="initTilt"
-        width="24px"
-        height="24px"
-    />
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24px"
+      height="24px"
+      viewBox="0 -960 960 960"
+      fill="#000000"
+    >
+      <path d="M200-120v-680h360l16 80h224v400H520l-16-80H280v280h-80Zm300-440Zm86 160h134v-240H510l-16-80H280v240h290l16 80Z"/>
+    </svg>
   </button>
 </template>
 
